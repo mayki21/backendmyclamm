9af51d1dac47e0c7a197306e61a742f6c03368a8
mayank <mayank.khobragade9891@gmail.com>
2023-07-25 12:53:33 +0530

    google wala

--- connection/Oauth.js
+++ connection/Oauth.js
@@ -15,7 +15,7 @@ passport.use(
     {
       clientID: client_id,
       clientSecret: client_secret,
-      callbackURL: "https://orchid-tuna-yoke.cyclic.app/auth/google/callback",
+      callbackURL: "http://localhost:8080/auth/google/callback",
     },
     async function (accessToken, refreshToken, profile, cb) {
       let user = {
